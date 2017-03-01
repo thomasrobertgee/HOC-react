f4b762d64b1aa170e31d5b0f505e6f07f9c3368c
Thomas Gee <thomasrobertgee@gmail.com>
2017-03-01 22:06:46 +1100

    tutorial app demonstrating react higher order components with authentication

---
+++ src/components/resources.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+export default () => {
+  return(
+    <div>
+      Super Special Secrets
+      <ul>
+        <li>secret one</li>
+        <li>secret two</li>
+        <li>secret three</li>
+        <li>secret four</li>
+        <li>secret six</li>
+      </ul>
+    </div>
+  )
+}
